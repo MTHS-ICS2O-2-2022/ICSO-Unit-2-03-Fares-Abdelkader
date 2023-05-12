616e9af039d82f143cebce025629baa83c584ff2
Fares-Abdelkader <124618781+Fares-Abdelkader@users.noreply.github.com>
2023-05-12 17:00:01 +0000

    Index.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Fares.Abdelkader
+// Created on: March 2022
 // This file contains the JS functions for index.html
+
+/**
+ * This function gets users first name and age and shows it back to user.
+ */
+function enterClicked() {
+  // input
+  const streetnumber = parseInt(document.getElementById("street-number-entered").value)
+  const streetname = document.getElementById("Street-name").value
+  // output
+  document.getElementById("address").innerHTML =
+    "Your address is: " + streetnumber + streetname + "."
+}
